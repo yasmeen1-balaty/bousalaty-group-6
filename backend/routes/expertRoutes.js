5f1fde4ffc5d1da961e6b35b71cb03bf5ba6d1fc
yazan_shawa <s12428589@stu.najah.edu>
2026-05-08 20:24:10 +0300

    add expert routs

---
+++ backend/routes/expertRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const expertController = require('../controllers/expertController');
+
+router.get('/:expertID', expertController.findExpert);
+router.post('/', expertController.createExpert);
+router.put('/:expertID', expertController.updateExpert);
+router.delete('/:expertID', expertController.deleteExpert);
+
+module.exports = router;
